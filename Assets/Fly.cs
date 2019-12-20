9114f6059e3297714f0d05a7b3ba84aa259025aa
aecio <aecio.jorge@gmail.com>
2019-12-20 15:27:39 -0400

    rotation in 3D space

--- Assets/Fly.cs
+++ Assets/Fly.cs
@@ -9,18 +9,32 @@ public class Fly : MonoBehaviour
 
     private void Update()
     {
-        float translateX = Input.GetAxis("Horizontal") * speed;
-        float translateY = Input.GetAxis("VerticalY") * speed;
-        float translateZ = Input.GetAxis("Vertical") * speed;
+        #region translate
+        // float translateX = Input.GetAxis("Horizontal") * speed;
+        // float translateY = Input.GetAxis("VerticalY") * speed;
+        // float translateZ = Input.GetAxis("Vertical") * speed;
 
-        //transform.Translate(translateX, 0, 0);
-        //transform.Translate(0, translateY, 0);
-        //transform.Translate(0, 0, translateZ);
+        // //transform.Translate(translateX, 0, 0);
+        // //transform.Translate(0, translateY, 0);
+        // //transform.Translate(0, 0, translateZ);
 
-        transform.position = new Vector3(transform.position.x + translateX,
-                                        transform.position.y + translateY,
-                                        transform.position.z + translateZ);
+        // transform.position = new Vector3(transform.position.x + translateX,
+        //                                 transform.position.y + translateY,
+        //                                 transform.position.z + translateZ);
 
-       // transform.Translate(translateX, translateY, translateZ);
+        //// transform.Translate(translateX, translateY, translateZ);
+        #endregion
+
+        float rotationX = Input.GetAxis("Vertical") * rotationSpeed;
+        float rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
+        float rotationZ = Input.GetAxis("HorizontalZ") * rotationSpeed;
+        float translateZ = Input.GetAxis("VerticalY") * rotationSpeed;
+
+        transform.Rotate(rotationX, 0, 0);
+        transform.Rotate(0, rotationY, 0);
+        transform.Rotate(0, 0, rotationZ);
+        transform.Translate(0, 0, translateZ);
+
+       // transform.Rotate(rotationX, rotationY, rotationZ);
     }
 }
